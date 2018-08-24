c199e20a746ae6b3b2ba0099b60989f0e9e9251d
Jesica Saussay <jesicagreenwell@gmail.com>
2018-08-24 16:13:20 +1000

    added uuid, styled components and atlaskit code

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,8 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-styled-components`,
+    }
   ],
 }
--- src/components/input/Input.story.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import Input from './Input'
-
-storiesOf('Input', module).add('default', () => (
-  <Input name="input-01" id="input-01" type="text" />
-))
